82629ffb8447994a798dee70a19ea8b7303d2f30
yung_denn <denn.03@mail.ru>
2021-10-28 18:14:15 +0500

    Add New Fragments: Add_Fragment and Ex..Fragment

---
+++ app/src/main/java/com/example/coinkeeperdemo/Add_fragment.kt
@@ -0,0 +1,38 @@
+package com.example.coinkeeperdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Add_fragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Add_fragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_fragment, container, false)
+    }
+
+    companion object {
+
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() = Add_fragment()
+
+    }
+}
